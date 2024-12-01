960bff9a194900e8451c90f53d92f8584ab29960
Hendra Hendriana <75461724+hendrahend@users.noreply.github.com>
2024-12-01 23:28:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+//Update for deployment
+module.exports = (req, res) => app(req, res);
