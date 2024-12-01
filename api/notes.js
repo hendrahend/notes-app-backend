7f1db6fb58ca19d2ad90b3e55330f28ef6de323b
hendrahend <hendh3565@gmail.com>
2024-12-02 00:04:39 +0700

    update

---
+++ api/notes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+// Routes
+app.get("/", async (req, res) => {
+  res.send("Halo..");
+});
+
+const noteRoutes = require("../routes/notes");
+app.use("/api/notes", noteRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+//Update for deployment
+module.exports = (req, res) => app(req, res);
