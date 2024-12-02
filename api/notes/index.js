4a7bf801e42d1fcc45b284623281fe555c1e0f26
hendrahend <hendh3565@gmail.com>
2024-12-02 13:49:57 +0700

    update folder structure & fixing for deployment

--- api/notes/index.js
+++ api/notes/index.js
@@ -21,8 +21,48 @@ mongoose
   });
 
 // Routes
-app.get("/", async (req, res) => {
-  res.send("Halo..");
+app.get('/', (req, res) => {
+  res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Notes App API</title>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  margin: 0;
+                  padding: 0;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 100vh;
+                  background-color: #f4f4f9;
+              }
+              .container {
+                  text-align: center;
+                  padding: 20px;
+                  background: white;
+                  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+                  border-radius: 8px;
+              }
+              h1 {
+                  color: #333;
+              }
+              p {
+                  color: #666;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <h2>Backend API for Notes App</h2>
+              <p>Go to <a href="https://notes-app-nine-tawny.vercel.app/">page</a></p>
+          </div>
+      </body>
+      </html>
+  `);
 });
 
 const noteRoutes = require("../../routes/notes");
