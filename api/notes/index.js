d8f516d6dcd24570e5c546ab748dcd07b56314af
hendrahend <hendh3565@gmail.com>
2024-12-02 11:18:41 +0700

    update

--- api/notes/index.js
+++ api/notes/index.js
@@ -33,6 +33,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-//Update for deployment
-export default (req, res) => app(req, res);
