c5e5dafd27a4ac78a26a5d62d7c62bbff8475111
hendrahend <hendh3565@gmail.com>
2024-12-02 11:46:22 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,8 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// Connect to MongoDB
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to MongoDB:", err);
+//   });
 
-module.exports = app;
+// Routes
+app.get("/", async (req, res) => {
+  res.send("Halo..");
+});
+
+// const noteRoutes = require("./routes/notes");
+// app.use("/api/notes", noteRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+//Update for deployment
+module.exports = (req, res) => app(req, res);
